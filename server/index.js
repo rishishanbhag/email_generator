c4edf48abe4a57eae2b415aa9ee28f572cf3584c
rishishanbhag <rishi.shanbhag2024@gmail.com>
2025-08-28 19:53:10 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected ✅");
+    app.listen(PORT, () => console.log("🚀 Server at http://localhost:3000"));
+  })
+  .catch((err) => console.error("❌ MongoDB error: ", err));
